0aa9cdb82bb6d21cb2ec93f8953d2a110abd0b69
tanitta <ttata.trit@gmail.com>
2024-05-21 09:44:02 +0900

    Enable to set index of input node referenced.

--- scripts/replace.py
+++ scripts/replace.py
@@ -6,17 +6,25 @@ def replace():
     parm = parms[0]
     pattern = parm.eval()
     node = parms[0].node()
-    geo = node.geometry()
+    referenced_input_index = 0
 
 
     attrName = "name"
-    Dialog = hou.ui.readInput(message ="Set Attribute Name To Search\n",title = "Set Attribute Name To Search",severity=hou.severityType.Warning,buttons=["Accept","Cancel"], initial_contents='name')
-    if Dialog[0]==0:
-        attrName = Dialog[1]
+    button_idx, values = hou.ui.readMultiInput(title = "Set Parms To Search Selected", message = "", buttons = ["Accept","Cancel"], severity = hou.severityType.Message, default_choice=0, close_choice=-1, help=None, 
+                                               input_labels     = ["Attribute Name", "Referenced Input"], 
+                                               initial_contents = (attrName,         str(referenced_input_index)))
+    if button_idx == 0:
+        attrName = values[0]
+        referenced_input_index = int(values[1])
     else:
         return
+    referenced_input_node = node.inputs()[referenced_input_index]
+    geo = referenced_input_node.geometry()
 
+    if button_idx == -1 or referenced_input_node is None:
+        geo = node.geometry()
     componentType = sys.argv[1]
+
     elems = []
     if componentType == "point":
         elems = geo.globPoints(pattern)
