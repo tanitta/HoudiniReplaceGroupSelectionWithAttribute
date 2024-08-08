b23f4610c21336c245d3321e9c42d318974138ce
tanitta <ttata.trit@gmail.com>
2024-08-08 22:10:41 +0900

    Enable to convert attr value to element index.

--- scripts/replace.py
+++ scripts/replace.py
@@ -7,23 +7,29 @@ def replace():
     pattern = parm.eval()
     node = parms[0].node()
     referenced_input_index = 0
-
+    componentType = sys.argv[1]
+    convert = sys.argv[2]
 
     attrName = "name"
-    button_idx, values = hou.ui.readMultiInput(title = "Set Parms To Search Selected", message = "", buttons = ["Accept","Cancel"], severity = hou.severityType.Message, default_choice=0, close_choice=-1, help=None, 
-                                               input_labels     = ["Attribute Name", "Referenced Input"], 
-                                               initial_contents = (attrName,         str(referenced_input_index)))
-    if button_idx == 0:
-        attrName = values[0]
-        referenced_input_index = int(values[1])
+
+    if convert == "attr":
+        button_idx, values = hou.ui.readMultiInput(title = "Set Parms To Search Selected", message = "", buttons = ["Accept","Cancel"], severity = hou.severityType.Message, default_choice=0, close_choice=-1, help=None, 
+                                                   input_labels     = ["Referenced Input",          "Attribute Name", ], 
+                                                   initial_contents = (str(referenced_input_index), attrName))
     else:
+        button_idx, values = hou.ui.readMultiInput(title = "Set Parms To Search Selected", message = "", buttons = ["Accept","Cancel"], severity = hou.severityType.Message, default_choice=0, close_choice=-1, help=None, 
+                                                   input_labels     = ["Referenced Input"], 
+                                                   initial_contents = (str(referenced_input_index)))
+    if button_idx != 0:
         return
+
+    referenced_input_index = int(values[0])
+
     referenced_input_node = node.inputs()[referenced_input_index]
     geo = referenced_input_node.geometry()
 
     if button_idx == -1 or referenced_input_node is None:
         geo = node.geometry()
-    componentType = sys.argv[1]
 
     elems = []
     if componentType == "point":
@@ -32,14 +38,21 @@ def replace():
         elems = geo.globVertices(pattern)
     elif componentType == "primitive":
         elems = geo.globPrims(pattern)
-
-    # find attr
-    values = set()
-    for elem in elems:
-        values.add(elem.attribValue(attrName))
-    parmStr = ""
-    for value in values:
-        parmStr += "@{}={} ".format(attrName, value)
-    parm.set(parmStr)
+    if convert == "attr":
+        attrName = values[1]
+        values = set()
+        for elem in elems:
+            values.add(elem.attribValue(attrName))
+        parm_lines = []
+        for value in values:
+            line = "@{}={}".format(attrName, value)
+            parm_lines.append(line)
+        parm.set('\n'.join(parm_lines))
+    else:
+        parm_lines = []
+        for elem in elems:
+            line = "{}".format(elem.number())
+            parm_lines.append(line)
+        parm.set(' '.join(parm_lines))
 
 replace()
